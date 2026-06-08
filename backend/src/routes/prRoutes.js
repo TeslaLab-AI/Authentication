1ddc61b08e6e93e78b189dc62e821e5d99824a48
Neetesh26 <neeteshprajapati009@gmail.com>
2026-06-08 20:57:32 +0530

    Initial commit

--- backend/src/routes/prRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { protect } from '../middleware/authMiddleware.js';
-import { approvePR } from '../controllers/prController.js';
-
-const router = express.Router();
-
-router.post('/:prId/approve', protect, approvePR);
-
-export default router;
